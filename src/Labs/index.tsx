ce47caa1395ba454d530ace3b0ec7dc6b6299a53
Michael Han <mhansnake@gmail.com>
2024-09-20 08:33:15 -0400

    finished

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,6 +8,10 @@ export default function Labs() {
     return (
         <div id="wd-lab">
             <h1>Labs</h1>
+            <h1>Michael Han</h1>
+            <h2>SEC 01</h2>
+            <h2>https://github.com/Michael-Hannn/kanbas-react-web-app.git</h2>
+
             <TOC />
             <Routes>
                 <Route path="Lab1" element={<Lab1 />} />
